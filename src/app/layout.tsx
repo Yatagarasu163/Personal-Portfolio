106ca9e4d3479ddf1c2ab530d32c70316cbba8e0
Yatagarasu163 <145321473+Yatagarasu163@users.noreply.github.com>
2025-04-24 23:03:28 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,27 @@
 import { ReactNode } from "react";
 import "./globals.css";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 // app/layout.tsx
 export const metadata = {
-  title: 'My Site',
-  description: 'A custom base layout',
+  title: 'Narish Kumar Dey - Portfolio',
+  description: 'A website that acts as my portfolio!',
 };
 
 export default function RootLayout({children} : {children: ReactNode}) {
+
   return (
     <html lang="en">
       <head>
           <meta charSet="UTF-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <title>My Personal Portfolio</title>
+          <title>Narish Kumar Dey - Portfolio</title>
       </head>
       <body>
           <Header />
           <main>{children}</main>
-          <footer>Narish Kumar Dey ©2025</footer>
+          <Footer />
       </body>
     </html>
   );
